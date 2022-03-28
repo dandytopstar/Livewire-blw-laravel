e2c10796ca420831de2cf421739e0aeb5cedb14d
dandytopstar <dandytopstar@gmail.com>
2022-03-28 21:40:57 +0300

    added scss and js-

--- public/js/app.js
+++ public/js/app.js
@@ -38594,6 +38594,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/front/app.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/front/app.scss ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./node_modules/process/browser.js":
 /*!*****************************************!*\
   !*** ./node_modules/process/browser.js ***!
@@ -38937,7 +38950,8 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
-/******/ 			"css/app": 0
+/******/ 			"css/app": 0,
+/******/ 			"front/css/app": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -38985,8 +38999,9 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","front/css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","front/css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","front/css/app"], () => (__webpack_require__("./resources/sass/front/app.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html dir="ltr" lang="en-US">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="author" content="Ayman Fikry"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <meta name="description" content="Multi-purpose Application landing page HTML5 template"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Landing CLick - Appzy HTML Landing Page Template</title>
     <link href="{{asset('front/assets/images/favicon/favicon.png')}}" rel="icon"/>
     <!--  Fonts -->
@@ -14,7 +15,7 @@
     <!--  Stylesheets-->
     <link href="{{asset('front/assets/css/vendor.min.css')}}" rel="stylesheet"/>
     <link href="{{asset('front/assets/css/style.css')}}" rel="stylesheet"/>
-    <link href="{{asset('css/app.css')}}" rel="stylesheet"/>
+    <link href="{{asset('front/css/app.css')}}" rel="stylesheet"/>
 </head>
 <body class="body-scroll">
 <!-- Document Wrapper-->
@@ -28,6 +29,6 @@
 <script src="{{asset('front/assets/js/vendor/jquery-3.4.1.min.js')}}"></script>
 <script src="{{asset('front/assets/js/vendor.min.js')}}"></script>
 <script src="{{asset('front/assets/js/functions.js')}}"></script>
-<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('front/js/app.js')}}"></script>
 </body>
 </html>
