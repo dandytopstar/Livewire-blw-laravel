da93289d791e39520f673c5883b1b0ee53599826
dandytopstar <dandytopstar@gmail.com>
2022-04-06 21:37:53 +0300

    start create quiz page

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -1,15 +1,34 @@
 @extends('layouts.front')
 
 @section('content')
-    @livewire('quiz')
-    <div class="d-flex flex-row quiz-to-bar">
-        <div class="align-self-start quiz-left-arrow"></div>
-        <div class="quiz-logo">
-            <img class="logo logo-light" src="{{'front/assets/images/logo/logo_blw.png'}}" alt="Appzy Logo" style="width: 350px"/>
+{{--    @livewire('quiz')--}}
+    <div class="quiz-top-bar">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-4 quiz-left-arrow">
+                    <i class="bi bi-arrow-left-circle quiz-top-bar-icon"></i>
+                </div>
+
+                <div class="col-4">
+                    <a href="/">
+                        <img class="quiz-logo" src="{{'front/assets/images/logo/logo_blw.png'}}" alt="Appzy Logo"/>
+                    </a>
+                </div>
+
+                <div class="col-4 quiz-right-arrow text--right">
+                    <i class="bi bi-arrow-right-circle quiz-top-bar-icon"></i>
+                </div>
+
+            </div>
         </div>
-        <div class="align-self-end quiz-right-arrow"></div>
     </div>
-    <div class="mx-auto w-50">
-        test
+
+    <div class="container mt-150">
+        <div class="mx-auto w-50">
+            <h2>I found it hard to wind down.</h2>
+        </div>
     </div>
+
+
 @endsection
