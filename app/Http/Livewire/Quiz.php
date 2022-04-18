45a64b734337ff052d66a04307afdbcaa133ac44
dandytopstar <dandytopstar@gmail.com>
2022-04-18 19:30:52 +0300

    fixed question number start

--- app/Http/Livewire/Quiz.php
+++ app/Http/Livewire/Quiz.php
@@ -343,7 +343,7 @@ class Quiz extends Component
 
         $this->countQuestions = count($this->quizQuestions);
 
-        $this->currentQuestionNum = 11;
+        $this->currentQuestionNum = 1;
 
         $this->currentQuestion = $this->quizQuestions[$this->currentQuestionNum];
 
