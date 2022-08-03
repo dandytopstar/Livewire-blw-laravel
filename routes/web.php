b76da9fe622f7ed52a1619cf13d611a9aa6c6c43
dandytopstar <dandytopstar@gmail.com>
2022-08-03 18:48:02 +0300

    Hide unecessary pages

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,29 @@ use App\Http\Controllers\PersonalPlanController;
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
-Route::view('/', 'main');
-Route::view('quiz', 'quiz')->name('quiz');
+//Route::get('/welcome', function () {
+//    return view('welcome');
+//});
 //Route::view('terms', 'terms')->name('terms');
 //Route::view('policy', 'policy')->name('policy');;
 
-Route::get('quiz-result/{code}', [QuizController::class, 'result'])->name('quiz-result');
-Route::get('summary/{code}', [QuizController::class, 'summary'])->name('summary');
-Route::get('checkout/{code}', [PersonalPlanController::class, 'checkout'])->name('checkout');
-Route::post('payment', [PaymentController::class, 'payment'])->name('payment');
-Route::get('payment-result/{id}/{code}', [PaymentController::class, 'paymentResult'])->name('payment-result');
-Route::get('paypal-success/{id}', [PaymentController::class, 'payPalSuccess'])->name('paypal-success');
-Route::get('paypal-error/{id}', [PaymentController::class, 'payPalError'])->name('paypal-error');
+Route::view('/', 'main');
 
+Route::view('quiz', 'quiz')->name('quiz');
+
+Route::get('quiz-result/{code}', [QuizController::class, 'result'])->name('quiz-result');
+
+Route::get('summary/{code}', [QuizController::class, 'summary'])->name('summary');
+
+Route::get('checkout/{code}', [PersonalPlanController::class, 'checkout'])->name('checkout');
+
+Route::post('payment', [PaymentController::class, 'payment'])->name('payment');
+
+Route::get('payment-result/{id}/{code}', [PaymentController::class, 'paymentResult'])->name('payment-result');
+
+Route::get('paypal-success/{id}', [PaymentController::class, 'payPalSuccess'])->name('paypal-success');
+
+Route::get('paypal-error/{id}', [PaymentController::class, 'payPalError'])->name('paypal-error');
 
 Route::get('send/home-question-form', [MailController::class, 'sendHomeQuestionForm'])
     ->name('send.home-question-form');
