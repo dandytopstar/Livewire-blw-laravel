a24a26a2ee888da83c8f917450a01616eaefbbe1
dandytopstar <dandytopstar@gmail.com>
2023-01-28 12:04:37 +0200

    added new GTAG

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @includeIf('partial.main-head')
 <body>
+<!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXLVV7S"
+                      height="0" width="0" style="display:none;visibility:hidden">
 
+        </iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @yield('content')
 
     @livewireScripts
