2d25e3a55a43ca3a9a3d8a57db1d5800dd15520a
dandytopstar <dandytopstar@gmail.com>
2023-04-03 20:11:40 +0300

    added hotjar and fixed UI

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -2,6 +2,18 @@
 
 @push('custom-header')
     <script src="https://www.paypal.com/sdk/js?client-id={{config('services.paypal.client_id')}}&vault=true&intent=subscription"></script>
+
+    <!-- Hotjar Tracking Code for my site -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3435383,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 @endpush
 
 @section('content')
@@ -171,7 +183,7 @@
                                                 </div>
                                             </div>
 
-                                            <div class="form">
+                                            <div class="form mt-4">
                                                 <form  action="" method="post" id="stripe-payment-form">
 
                                                     @csrf
