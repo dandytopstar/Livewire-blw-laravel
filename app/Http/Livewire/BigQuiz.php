1fa1084c2749a9a28ba8878c7d9725795ae2a3f4
dandytopstar <dandytopstar@gmail.com>
2023-05-02 18:40:21 +0300

    start from 1 question

--- app/Http/Livewire/BigQuiz.php
+++ app/Http/Livewire/BigQuiz.php
@@ -443,7 +443,7 @@ class BigQuiz extends Component
 
         $this->countQuestions = count($this->quizQuestions);
 
-        $this->currentQuestionNum = 25;
+        $this->currentQuestionNum = 1;
 
         $this->currentQuestion = $this->quizQuestions[$this->currentQuestionNum];
 
