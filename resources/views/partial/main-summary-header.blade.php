177b76c70571f634cc402d722adb99fa3e5b5513
dandytopstar <dandytopstar@gmail.com>
2023-05-07 14:07:40 +0300

    fixed header adaptive

--- resources/views/partial/main-summary-header.blade.php
+++ resources/views/partial/main-summary-header.blade.php
@@ -3,11 +3,11 @@
         <div class="summary-header-logo" style="flex-wrap: wrap;">
             <img src="{{asset('assets/logo/main-logo.png')}}" alt="" class="desktop-logo">
             <div>
-                <img class="adaptive" src="{{asset('assets/logo/main-logo.png')}}" alt="">
+                <img class="adaptive" src="{{asset('assets/summary/adaptive_logo.png')}}" alt="">
             </div>
 
             <div class="summary-header-btn">
-                <a href="{{route('registration', $code)}}" type="button" class="btn font-white-600 btn-green-squre">
+                <a href="{{route('registration', $code)}}" type="button" class="btn font-white-600 btn-green-squre btn-adaptive">
                     {{__('front.get_your_plan')}}
                 </a>
             </div>
